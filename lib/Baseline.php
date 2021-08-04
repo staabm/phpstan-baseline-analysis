14b94649825f172fc8e4667b7c6d984be3e49807
Markus Staab <m.staab@complex-it.de>
2021-08-04 16:04:30 +0200

    improve error handling

--- lib/Baseline.php
+++ lib/Baseline.php
@@ -2,7 +2,9 @@
 
 namespace staabm\PHPStanBaselineAnalysis;
 
+use Iterator;
 use Nette\Neon\Neon;
+use RuntimeException;
 
 final class Baseline {
     /**
@@ -21,7 +23,7 @@ final class Baseline {
         $decoded = Neon::decode($content);
 
         if (!is_array($decoded)) {
-            throw new \RuntimeException(sprintf('expecting baseline %s to be non-empty', $filePath));
+            throw new RuntimeException(sprintf('expecting baseline %s to be non-empty', $filePath));
         }
 
         $baseline = new self();
@@ -33,14 +35,14 @@ final class Baseline {
     /**
      * @return Iterator<string>
      */
-    public function getIgnoreErrors() {
-        if (!array_key_exists('parameters', $this->content)) {
-            throw new \RuntimeException('missing paramters from baseline');
+    public function getIgnoreErrors(): Iterator {
+        if (!array_key_exists('parameters', $this->content) || !is_array($this->content['parameters'])) {
+            throw new RuntimeException(sprintf('missing paramters from baseline %s', $this->filePath));
         }
         $parameters = $this->content['parameters'];
 
-        if (!array_key_exists('ignoreErrors', $parameters)) {
-            throw new \RuntimeException('missing ignoreErrors from baseline');
+        if (!array_key_exists('ignoreErrors', $parameters) || !is_array($parameters['ignoreErrors'])) {
+            throw new RuntimeException(sprintf('missing ignoreErrors from baseline %s', $this->filePath));
         }
         $ignoreErrors = $parameters['ignoreErrors'];
 
