be66222e4b34a38b8ec0be22769184ceac39233c
Markus Staab <maggus.staab@googlemail.com>
2021-08-04 16:06:45 +0200

    support recursive glob (#1)
    Co-authored-by: Markus Staab <m.staab@complex-it.de>

--- lib/BaselineFinder.php
+++ lib/BaselineFinder.php
@@ -9,11 +9,11 @@ final class BaselineFinder
     /**
      * @return Baseline[]
      */
-    static public function forGlob(string $glob):array
+    static public function forGlob(string $glob): array
     {
         $baselines = [];
 
-        foreach (glob($glob) as $baseline) {
+        foreach (self::rglob($glob) as $baseline) {
             if (!is_file($baseline)) {
                 continue;
             }
@@ -26,5 +26,15 @@ final class BaselineFinder
         }
 
         return $baselines;
+    }
+
+    // from https://stackoverflow.com/a/17161106
+    static private function rglob($pattern, $flags = 0)
+    {
+        $files = glob($pattern, $flags);
+        foreach (glob(dirname($pattern) . '/*', GLOB_ONLYDIR | GLOB_NOSORT) as $dir) {
+            $files = array_merge($files, self::rglob($dir . '/' . basename($pattern), $flags));
+        }
+        return $files;
     }
 }
