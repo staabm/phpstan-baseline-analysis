b4abf2f888a8f95518e8045998ea99290bfd36ce
Markus Staab <maggus.staab@googlemail.com>
2022-09-30 14:27:33 +0200

    extract AnalyzerResultReader (#72)
    Co-authored-by: Markus Staab <m.staab@complex-it.de>

---
+++ lib/AnalyzerResultReader.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace staabm\PHPStanBaselineAnalysis;
+
+use function Safe\file_get_contents;
+use function Safe\json_decode;
+
+final class AnalyzerResultReader {
+    /**
+     * @return array<string, AnalyzerResult>
+     * @throws \Safe\Exceptions\FilesystemException
+     * @throws \Safe\Exceptions\JsonException
+     */
+    public function readFile(string $filePath): array
+    {
+        $content = file_get_contents($filePath);
+        $json = json_decode($content, true);
+
+        if (!is_array($json)) {
+            throw new \RuntimeException('Expecting array, got ' . gettype($json));
+        }
+
+        $decoded = [];
+        foreach ($json as $data) {
+
+            if (!is_array($data)) {
+                throw new \RuntimeException('Expecting array, got ' . gettype($data));
+            }
+
+            foreach ($data as $baselinePath => $resultArray) {
+
+                if (!is_string($baselinePath)) {
+                    throw new \RuntimeException('Expecting string, got ' . gettype($baselinePath));
+                }
+                if (!is_array($resultArray)) {
+                    throw new \RuntimeException('Expecting string, got ' . gettype($resultArray));
+                }
+
+                $result = new AnalyzerResult();
+                if (array_key_exists(ResultPrinter::KEY_OVERALL_ERRORS, $resultArray)) {
+                    $result->overallErrors = $resultArray[ResultPrinter::KEY_OVERALL_ERRORS];
+                }
+                if (array_key_exists(ResultPrinter::KEY_CLASSES_COMPLEXITY, $resultArray)) {
+                    $result->classesComplexity = $resultArray[ResultPrinter::KEY_CLASSES_COMPLEXITY];
+                }
+                if (array_key_exists(ResultPrinter::KEY_DEPRECATIONS, $resultArray)) {
+                    $result->deprecations = $resultArray[ResultPrinter::KEY_DEPRECATIONS];
+                }
+                if (array_key_exists(ResultPrinter::KEY_INVALID_PHPDOCS, $resultArray)) {
+                    $result->invalidPhpdocs = $resultArray[ResultPrinter::KEY_INVALID_PHPDOCS];
+                }
+                if (array_key_exists(ResultPrinter::KEY_UNKNOWN_TYPES, $resultArray)) {
+                    $result->unknownTypes = $resultArray[ResultPrinter::KEY_UNKNOWN_TYPES];
+                }
+                if (array_key_exists(ResultPrinter::KEY_ANONYMOUS_VARIABLES, $resultArray)) {
+                    $result->anonymousVariables = $resultArray[ResultPrinter::KEY_ANONYMOUS_VARIABLES];
+                }
+
+                $decoded[$baselinePath] = $result;
+            }
+        }
+
+        return $decoded;
+    }
+}
