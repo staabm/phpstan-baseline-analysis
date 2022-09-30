8d0a9f0252d90ea423bcb9cf561bc0145c85ab81
Markus Staab <m.staab@complex-it.de>
2022-09-30 14:25:46 +0200

    extract AnalyzerResultReader

--- lib/TrendApplication.php
+++ lib/TrendApplication.php
@@ -22,8 +22,9 @@ final class TrendApplication
     {
         $exitCode = self::EXIT_IMPROVED;
 
-        $reference = $this->decodeFile($referenceFilePath);
-        $comparing = $this->decodeFile($comparingFilePath);
+        $reader = new AnalyzerResultReader();
+        $reference = $reader->readFile($referenceFilePath);
+        $comparing = $reader->readFile($comparingFilePath);
 
         foreach ($reference as $baselinePath => $result) {
             echo 'Analyzing Trend for ' . $baselinePath . "\n";
@@ -55,63 +56,6 @@ final class TrendApplication
     public function help(): void
     {
         printf('USAGE: phpstan-baseline-trend <reference-result.json> <comparing-result.json>');
-    }
-
-    /**
-     * @return array<string, AnalyzerResult>
-     * @throws \Safe\Exceptions\FilesystemException
-     * @throws \Safe\Exceptions\JsonException
-     */
-    private function decodeFile(string $filePath): array
-    {
-        $content = file_get_contents($filePath);
-        $json = json_decode($content, true);
-
-        if (!is_array($json)) {
-            throw new \RuntimeException('Expecting array, got ' . gettype($json));
-        }
-
-        $decoded = [];
-        foreach ($json as $data) {
-
-            if (!is_array($data)) {
-                throw new \RuntimeException('Expecting array, got ' . gettype($data));
-            }
-
-            foreach ($data as $baselinePath => $resultArray) {
-
-                if (!is_string($baselinePath)) {
-                    throw new \RuntimeException('Expecting string, got ' . gettype($baselinePath));
-                }
-                if (!is_array($resultArray)) {
-                    throw new \RuntimeException('Expecting string, got ' . gettype($resultArray));
-                }
-
-                $result = new AnalyzerResult();
-                if (array_key_exists(ResultPrinter::KEY_OVERALL_ERRORS, $resultArray)) {
-                    $result->overallErrors = $resultArray[ResultPrinter::KEY_OVERALL_ERRORS];
-                }
-                if (array_key_exists(ResultPrinter::KEY_CLASSES_COMPLEXITY, $resultArray)) {
-                    $result->classesComplexity = $resultArray[ResultPrinter::KEY_CLASSES_COMPLEXITY];
-                }
-                if (array_key_exists(ResultPrinter::KEY_DEPRECATIONS, $resultArray)) {
-                    $result->deprecations = $resultArray[ResultPrinter::KEY_DEPRECATIONS];
-                }
-                if (array_key_exists(ResultPrinter::KEY_INVALID_PHPDOCS, $resultArray)) {
-                    $result->invalidPhpdocs = $resultArray[ResultPrinter::KEY_INVALID_PHPDOCS];
-                }
-                if (array_key_exists(ResultPrinter::KEY_UNKNOWN_TYPES, $resultArray)) {
-                    $result->unknownTypes = $resultArray[ResultPrinter::KEY_UNKNOWN_TYPES];
-                }
-                if (array_key_exists(ResultPrinter::KEY_ANONYMOUS_VARIABLES, $resultArray)) {
-                    $result->anonymousVariables = $resultArray[ResultPrinter::KEY_ANONYMOUS_VARIABLES];
-                }
-
-                $decoded[$baselinePath] = $result;
-            }
-        }
-
-        return $decoded;
     }
 
     /**
