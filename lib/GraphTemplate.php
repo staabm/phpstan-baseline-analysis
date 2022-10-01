3f8158e05961c679039dc31b898c71630bc59175
Markus Staab <m.staab@complex-it.de>
2022-10-01 12:28:50 +0200

    support multiple graphs on a single page

--- lib/GraphTemplate.php
+++ lib/GraphTemplate.php
@@ -13,64 +13,67 @@ final class GraphTemplate
      */
     public function render(Iterator $it):string {
         $splines = [];
-        $splines[ResultPrinter::KEY_OVERALL_ERRORS] = [];
-        $splines[ResultPrinter::KEY_CLASSES_COMPLEXITY] = [];
-        $splines[ResultPrinter::KEY_DEPRECATIONS] = [];
-        $splines[ResultPrinter::KEY_INVALID_PHPDOCS] = [];
-        $splines[ResultPrinter::KEY_UNKNOWN_TYPES] = [];
-        $splines[ResultPrinter::KEY_ANONYMOUS_VARIABLES] = [];
         foreach($it as $data) {
             /** @var AnalyzerResult $analyzerResult */
             list($baselinePath, $analyzerResult) = $data;
-            // XXX timestamp aus result verwenden
+
             if ($analyzerResult->referenceDate === null) {
                 continue;
             }
             $timestamp = $analyzerResult->referenceDate->getTimestamp();
 
-            $splines[ResultPrinter::KEY_OVERALL_ERRORS][] = '{x: new Date('. $timestamp.' * 1000), y: '.$analyzerResult->overallErrors.'}';
-            $splines[ResultPrinter::KEY_CLASSES_COMPLEXITY][] = '{x: new Date('. $timestamp.' * 1000), y: '.$analyzerResult->classesComplexity.'}';
-            $splines[ResultPrinter::KEY_DEPRECATIONS][] = '{x: new Date('. $timestamp.' * 1000), y: '.$analyzerResult->deprecations.'}';
-            $splines[ResultPrinter::KEY_INVALID_PHPDOCS][] = '{x: new Date('. $timestamp.' * 1000), y: '.$analyzerResult->invalidPhpdocs.'}';
-            $splines[ResultPrinter::KEY_UNKNOWN_TYPES][] = '{x: new Date('. $timestamp.' * 1000), y: '.$analyzerResult->unknownTypes.'}';
-            $splines[ResultPrinter::KEY_ANONYMOUS_VARIABLES][] = '{x: new Date('. $timestamp.' * 1000), y: '.$analyzerResult->anonymousVariables.'}';
-        }
+            if (!array_key_exists($baselinePath, $splines)) {
+                $splines[$baselinePath] = [];
 
-        $jsData = [];
-        foreach($splines as $name => $dataPoints) {
-            $type = 'line';
-            $complexityProps = '';
-            $axisYIndex = 0;
-            if ($name == ResultPrinter::KEY_CLASSES_COMPLEXITY) {
-                $type = 'spline';
-                $complexityProps = 'color: "'. self::COMPLEXITY_COLOR .'", lineThickness: 4, axisYType: "secondary",';
-                $axisYIndex = 1;
+                $splines[$baselinePath][ResultPrinter::KEY_OVERALL_ERRORS] = [];
+                $splines[$baselinePath][ResultPrinter::KEY_CLASSES_COMPLEXITY] = [];
+                $splines[$baselinePath][ResultPrinter::KEY_DEPRECATIONS] = [];
+                $splines[$baselinePath][ResultPrinter::KEY_INVALID_PHPDOCS] = [];
+                $splines[$baselinePath][ResultPrinter::KEY_UNKNOWN_TYPES] = [];
+                $splines[$baselinePath][ResultPrinter::KEY_ANONYMOUS_VARIABLES] = [];
             }
 
-            $jsData[] = '{
-                type: "'.$type.'",
-                name: "'.$name.'",
-                axisYIndex: '. $axisYIndex.',
-                '. $complexityProps .'
-                showInLegend: true,
-                xValueType: "dateTime",
-                dataPoints: ['. implode(',', $dataPoints) .']
-            }';
+            $splines[$baselinePath][ResultPrinter::KEY_OVERALL_ERRORS][] = '{x: new Date('. $timestamp.' * 1000), y: '.$analyzerResult->overallErrors.'}';
+            $splines[$baselinePath][ResultPrinter::KEY_CLASSES_COMPLEXITY][] = '{x: new Date('. $timestamp.' * 1000), y: '.$analyzerResult->classesComplexity.'}';
+            $splines[$baselinePath][ResultPrinter::KEY_DEPRECATIONS][] = '{x: new Date('. $timestamp.' * 1000), y: '.$analyzerResult->deprecations.'}';
+            $splines[$baselinePath][ResultPrinter::KEY_INVALID_PHPDOCS][] = '{x: new Date('. $timestamp.' * 1000), y: '.$analyzerResult->invalidPhpdocs.'}';
+            $splines[$baselinePath][ResultPrinter::KEY_UNKNOWN_TYPES][] = '{x: new Date('. $timestamp.' * 1000), y: '.$analyzerResult->unknownTypes.'}';
+            $splines[$baselinePath][ResultPrinter::KEY_ANONYMOUS_VARIABLES][] = '{x: new Date('. $timestamp.' * 1000), y: '.$analyzerResult->anonymousVariables.'}';
         }
 
-        return '
-            <!DOCTYPE HTML>
-            <html>
-            <head>
-                <script>
-                    window.onload = function () {
-            
-                        var chart = new CanvasJS.Chart("chartContainer", {
+        $chartsHtml = '';
+        foreach($splines as $baselinePath => $data) {
+            $jsData = [];
+            foreach ($data as $name => $dataPoints) {
+                $type = 'line';
+                $complexityProps = '';
+                $axisYIndex = 0;
+                if ($name == ResultPrinter::KEY_CLASSES_COMPLEXITY) {
+                    $type = 'spline';
+                    $complexityProps = 'color: "' . self::COMPLEXITY_COLOR . '", lineThickness: 4, axisYType: "secondary",';
+                    $axisYIndex = 1;
+                }
+
+                $jsData[] = '{
+                    type: "' . $type . '",
+                    name: "' . $name . '",
+                    axisYIndex: ' . $axisYIndex . ',
+                    ' . $complexityProps . '
+                    showInLegend: true,
+                    xValueType: "dateTime",
+                    dataPoints: [' . implode(',', $dataPoints) . ']
+                }';
+            }
+            $chartsHtml .= '
+                    <div id="chartContainer'. md5($baselinePath).'" style="height: 370px; width: 100%; margin-bottom: 20px;"></div>
+                    <script>
+                    (function () {
+                        var chart = new CanvasJS.Chart("chartContainer'. md5($baselinePath).'", {
                             animationEnabled: true,
                             title:{
-                                text: "PHPStan Baseline Analysis"
+                                text: "PHPStan Baseline Analysis '. $baselinePath .'"
                             },
-                            	toolTip: {
+                                toolTip: {
                                 shared: true
                             },
                             axisX: {
@@ -91,13 +94,18 @@ final class GraphTemplate
                         });
             
                         chart.render();
-            
-                    }
+                    })();
                 </script>
-            </head>
+';
+        }
+
+        return '
+            <!DOCTYPE HTML>
+            <html>
+            <head></head>
             <body>
-            <div id="chartContainer" style="height: 370px; width: 100%;"></div>
-            <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+                <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+                '. $chartsHtml.'
             </body>
             </html>
         ';
