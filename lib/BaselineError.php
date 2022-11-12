1d9a05b4f0bd7543cb5d04d6a50c5d2f760c4003
Markus Staab <maggus.staab@googlemail.com>
2022-11-12 10:15:51 +0100

    end2end test with symplify message format

--- lib/BaselineError.php
+++ lib/BaselineError.php
@@ -13,4 +13,14 @@ final class BaselineError
      * @var string
      */
     public $message;
+
+    /**
+     * Returns the baseline error message, without regex delimiters.
+     * Note: the message may still contain escaped regex meta characters.
+     * 
+     * @return string
+     */
+    public function unwrapMessage(): string {
+        return trim($this->message, '#^$');
+    }
 }
