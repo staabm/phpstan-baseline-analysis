7c642de306351e50ad41851521900b5c72d8314b
Markus Staab <maggus.staab@googlemail.com>
2022-11-12 22:12:29 +0100

    fix

--- tests/GraphApplicationTest.php
+++ tests/GraphApplicationTest.php
@@ -19,7 +19,7 @@ class GraphApplicationTest extends BaseTestCase
         $rendered = ob_get_clean();
 
         // file_put_contents(__DIR__ . '/fixtures/graph.html.expected', $rendered);
-        file_put_contents(__DIR__ . '/fixtures/graph.html', $rendered);
+        // file_put_contents(__DIR__ . '/fixtures/graph.html', $rendered);
         $this->assertSame(file_get_contents(__DIR__ . '/fixtures/graph.html.expected'), $rendered);
         $this->assertSame(0, $exitCode);
     }
