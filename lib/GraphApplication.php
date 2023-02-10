f82098441d2a13257775fdb480f45e56605f9523
Markus Staab <maggus.staab@googlemail.com>
2023-02-10 18:44:02 +0100

    Update GraphApplication.php

--- lib/GraphApplication.php
+++ lib/GraphApplication.php
@@ -17,7 +17,7 @@ final class GraphApplication
     public function start(string $jsonGlob): int
     {
         $jsonFiles = glob($jsonGlob, GLOB_NOSORT);
-        \Safe\usort( $jsonFiles, function( $a, $b ) {
+        \Safe\usort( $jsonFiles, function( string $a, string $b ) {
             return \Safe\filemtime($a) - \Safe\filemtime($b);
         });
 
