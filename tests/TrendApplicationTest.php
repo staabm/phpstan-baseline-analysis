fe2fed84ce020652d3145b8dcb241daa173e0a9c
Manuel Schmidt <m.schmidt@complex-it.de>
2023-06-26 07:07:41 +0200

    fix test: empty spaces seems to be a problem while comparing

--- tests/TrendApplicationTest.php
+++ tests/TrendApplicationTest.php
@@ -18,13 +18,13 @@ class TrendApplicationTest extends BaseTestCase
 
         $expected = <<<PHP
 Analyzing Trend for /fixtures/all-in.neon
-  Overall-Errors: 18 -> 18 => good
-  Classes-Cognitive-Complexity: 70 -> 70 => good
-  Deprecations: 1 -> 1 => good
-  Invalid-Phpdocs: 3 -> 3 => good
-  Unknown-Types: 5 -> 5 => good
-  Anonymous-Variables: 2 -> 2 => good
-  Unused-Symbols: 1 -> 1 => good
+Overall-Errors: 18 -> 18 => good
+Classes-Cognitive-Complexity: 70 -> 70 => good
+Deprecations: 1 -> 1 => good
+Invalid-Phpdocs: 3 -> 3 => good
+Unknown-Types: 5 -> 5 => good
+Anonymous-Variables: 2 -> 2 => good
+Unused-Symbols: 1 -> 1 => good
 
 PHP;
 
@@ -44,13 +44,13 @@ PHP;
 
         $expected = <<<PHP
 Analyzing Trend for /fixtures/all-in.neon
-  Overall-Errors: 18 -> 24 => worse
-  Classes-Cognitive-Complexity: 70 -> 90 => worse
-  Deprecations: 1 -> 10 => worse
-  Invalid-Phpdocs: 3 -> 30 => worse
-  Unknown-Types: 5 -> 15 => worse
-  Anonymous-Variables: 2 -> 5 => worse
-  Unused-Symbols: 1 -> 10 => worse
+Overall-Errors: 18 -> 24 => worse
+Classes-Cognitive-Complexity: 70 -> 90 => worse
+Deprecations: 1 -> 10 => worse
+Invalid-Phpdocs: 3 -> 30 => worse
+Unknown-Types: 5 -> 15 => worse
+Anonymous-Variables: 2 -> 5 => worse
+Unused-Symbols: 1 -> 10 => worse
 
 PHP;
 
@@ -70,13 +70,13 @@ PHP;
 
         $expected = <<<PHP
 Analyzing Trend for /fixtures/all-in.neon
-  Overall-Errors: 18 -> 10 => improved
-  Classes-Cognitive-Complexity: 70 -> 50 => improved
-  Deprecations: 1 -> 0 => improved
-  Invalid-Phpdocs: 3 -> 1 => improved
-  Unknown-Types: 5 -> 3 => improved
-  Anonymous-Variables: 2 -> 1 => improved
-  Unused-Symbols: 1 -> 0 => improved
+Overall-Errors: 18 -> 10 => improved
+Classes-Cognitive-Complexity: 70 -> 50 => improved
+Deprecations: 1 -> 0 => improved
+Invalid-Phpdocs: 3 -> 1 => improved
+Unknown-Types: 5 -> 3 => improved
+Anonymous-Variables: 2 -> 1 => improved
+Unused-Symbols: 1 -> 0 => improved
 
 PHP;
 
@@ -96,7 +96,6 @@ PHP;
 
         $expected = <<<PHP
 [{"headline":"Analyzing Trend for \/fixtures\/all-in.neon","results":{"Overall-Errors":{"reference":18,"comparing":18,"trend":"good"},"Classes-Cognitive-Complexity":{"reference":70,"comparing":70,"trend":"good"},"Deprecations":{"reference":1,"comparing":1,"trend":"good"},"Invalid-Phpdocs":{"reference":3,"comparing":3,"trend":"good"},"Unknown-Types":{"reference":5,"comparing":5,"trend":"good"},"Anonymous-Variables":{"reference":2,"comparing":2,"trend":"good"},"Unused-Symbols":{"reference":1,"comparing":1,"trend":"good"}}}]
-
 PHP;
 
         $this->assertSame($expected, $rendered);
@@ -115,7 +114,6 @@ PHP;
 
         $expected = <<<PHP
 [{"headline":"Analyzing Trend for \/fixtures\/all-in.neon","results":{"Overall-Errors":{"reference":18,"comparing":24,"trend":"worse"},"Classes-Cognitive-Complexity":{"reference":70,"comparing":90,"trend":"worse"},"Deprecations":{"reference":1,"comparing":10,"trend":"worse"},"Invalid-Phpdocs":{"reference":3,"comparing":30,"trend":"worse"},"Unknown-Types":{"reference":5,"comparing":15,"trend":"worse"},"Anonymous-Variables":{"reference":2,"comparing":5,"trend":"worse"},"Unused-Symbols":{"reference":1,"comparing":10,"trend":"worse"}}}]
-
 PHP;
 
         $this->assertSame($expected, $rendered);
@@ -134,7 +132,6 @@ PHP;
 
         $expected = <<<PHP
 [{"headline":"Analyzing Trend for \/fixtures\/all-in.neon","results":{"Overall-Errors":{"reference":18,"comparing":10,"trend":"improved"},"Classes-Cognitive-Complexity":{"reference":70,"comparing":50,"trend":"improved"},"Deprecations":{"reference":1,"comparing":0,"trend":"improved"},"Invalid-Phpdocs":{"reference":3,"comparing":1,"trend":"improved"},"Unknown-Types":{"reference":5,"comparing":3,"trend":"improved"},"Anonymous-Variables":{"reference":2,"comparing":1,"trend":"improved"},"Unused-Symbols":{"reference":1,"comparing":0,"trend":"improved"}}}]
-
 PHP;
 
         $this->assertSame($expected, $rendered);
