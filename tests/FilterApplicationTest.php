d6dbfb6c0bbd5e09c3810ba03e08aafa303a5a01
fbecker-complex <71324952+fbecker-complex@users.noreply.github.com>
2023-10-26 11:38:21 +0200

    test printing of filter result

--- tests/FilterApplicationTest.php
+++ tests/FilterApplicationTest.php
@@ -7,6 +7,30 @@ use staabm\PHPStanBaselineAnalysis\ResultPrinter;
 
 final class FilterApplicationTest extends BaseTestCase
 {
+    function testTextPrinting():void
+    {
+        $app = new FilterApplication();
+
+        ob_start();
+        $exitCode = $app->start(__DIR__ . '/fixtures/all-in.neon', ResultPrinter::KEY_DEPRECATIONS);
+        $rendered = ob_get_clean();
+
+        $rendered = str_replace(__DIR__, '', $rendered);
+
+        $expected = <<<PHP
+parameters:
+	ignoreErrors:
+		-
+			message: "#^Instantiation of deprecated class Zend_Db_Expr\\.$#"
+			count: 2
+			path: controllers/AccountWatchlistController.php
+
+PHP;
+
+        $this->assertSame($expected, $rendered);
+        $this->assertSame(0, $exitCode);
+    }
+
     function testNoMatchingGlob():void
     {
         $app = new FilterApplication();
