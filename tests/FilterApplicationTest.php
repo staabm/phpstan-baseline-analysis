d983081acf8ae1e3959572ff03c7dda50395be13
Markus Staab <maggus.staab@googlemail.com>
2023-10-30 08:22:52 +0100

    another test

--- tests/FilterApplicationTest.php
+++ tests/FilterApplicationTest.php
@@ -17,13 +17,47 @@ final class FilterApplicationTest extends BaseTestCase
 
         $rendered = str_replace(__DIR__, '', $rendered);
 
-        $expected = <<<PHP
+        $expected = <<<'PHP'
 parameters:
 	ignoreErrors:
 		-
 			message: "#^Instantiation of deprecated class Zend_Db_Expr\\.$#"
 			count: 2
 			path: controllers/AccountWatchlistController.php
+
+PHP;
+
+        $this->assertSame($expected, $rendered);
+        $this->assertSame(0, $exitCode);
+    }
+
+    function testUnusedTypes():void
+    {
+        $app = new FilterApplication();
+
+        ob_start();
+        $exitCode = $app->start(__DIR__ . '/fixtures/all-in.neon', ResultPrinter::KEY_UNUSED_SYMBOLS);
+        $rendered = ob_get_clean();
+
+        $rendered = str_replace(__DIR__, '', $rendered);
+
+        $expected = <<<'PHP'
+parameters:
+	ignoreErrors:
+		-
+			message: "#^Public method \"clxImage\\:\\:upload\\(\\)\" is never used$#"
+			count: 1
+			path: app/admin/models/clxImage.php
+
+		-
+			message: "#^Public property \"event\\\\persistence\\\\EventTimeslotRecord\\:\\:\\$time_from\" is never used$#"
+			count: 1
+			path: app/admin/lib/event/persistence/EventTimeslotRecord.php
+
+		-
+			message: "#^Public constant \"SearchExport\\:\\:TMP_PATH\" is never used$#"
+			count: 1
+			path: scripts/portal/SearchExport.php
 
 PHP;
 
