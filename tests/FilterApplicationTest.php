8a357a49303f8a047ea8aeb6a0a5d812c1f71030
Markus Staab <maggus.staab@googlemail.com>
2023-10-30 09:59:22 +0100

    test multi include

--- tests/FilterApplicationTest.php
+++ tests/FilterApplicationTest.php
@@ -8,7 +8,7 @@ use staabm\PHPStanBaselineAnalysis\ResultPrinter;
 
 final class FilterApplicationTest extends BaseTestCase
 {
-    function testTextPrinting():void
+    function testSingleInclude():void
     {
         $app = new FilterApplication();
 
@@ -26,6 +26,46 @@ parameters:
 			message: '#^Instantiation of deprecated class Zend_Db_Expr\.$#'
 			count: 2
 			path: controllers/AccountWatchlistController.php
+
+PHP;
+
+        $this->assertSame($expected, $rendered);
+        $this->assertSame(0, $exitCode);
+    }
+
+    function testMultiInclude():void
+    {
+        $app = new FilterApplication();
+
+        ob_start();
+        $fiterConfig = FilterConfig::fromArgs('--include='.ResultPrinter::KEY_DEPRECATIONS.','.ResultPrinter::KEY_UNUSED_SYMBOLS);
+        $exitCode = $app->start(__DIR__ . '/fixtures/all-in.neon', $fiterConfig);
+        $rendered = ob_get_clean();
+
+        $rendered = str_replace(__DIR__, '', $rendered);
+
+        $expected = <<<'PHP'
+parameters:
+	ignoreErrors:
+		-
+			message: '#^Instantiation of deprecated class Zend_Db_Expr\.$#'
+			count: 2
+			path: controllers/AccountWatchlistController.php
+
+		-
+			message: '#^Public method "clxImage\:\:upload\(\)" is never used$#'
+			count: 1
+			path: app/admin/models/clxImage.php
+
+		-
+			message: '#^Public property "event\\persistence\\EventTimeslotRecord\:\:\$time_from" is never used$#'
+			count: 1
+			path: app/admin/lib/event/persistence/EventTimeslotRecord.php
+
+		-
+			message: '#^Public constant "SearchExport\:\:TMP_PATH" is never used$#'
+			count: 1
+			path: scripts/portal/SearchExport.php
 
 PHP;
 
